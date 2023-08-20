335347879b6df96bd8b1d27585f8363fd6f65e12
Shinichi Okada <okada.shin@gmail.com>
2023-08-20 08:00:49 +0200

    feat: update index

--- index.js
+++ index.js
@@ -5,44 +5,44 @@ import path from 'path';
 
 
 const addCompoDocs = () => {
-const srcDir = path.join(__dirname, 'src', 'lib');
+  const srcDir = path.join(__dirname, 'src', 'lib');
 
-const processFile = (filePath) => {
-  let content = fs.readFileSync(filePath, 'utf-8');
+  const processFile = (filePath) => {
+    let content = fs.readFileSync(filePath, 'utf-8');
 
-  // Remove previous @component comments and trailing empty lines
-  content = content.replace(/<!--\s*@component[\s\S]*?-->\n*/, '').replace(/\s*$/, '');
+    // Remove previous @component comments and trailing empty lines
+    content = content.replace(/<!--\s*@component[\s\S]*?-->\n*/, '').replace(/\s*$/, '');
 
-  // Find all lines starting with 'export let' and create @prop comments
-  const propLines = content.match(/export let [\s\S]*?;/g);
-  if (propLines) {
-    const propComment = propLines.map(line => {
-      return `@prop ${line.replace(/\/\/.*$/, '').trim()}`;
-    }).join('\n');
+    // Find all lines starting with 'export let' and create @prop comments
+    const propLines = content.match(/export let [\s\S]*?;/g);
+    if (propLines) {
+      const propComment = propLines.map(line => {
+        return `@prop ${line.replace(/\/\/.*$/, '').trim()}`;
+      }).join('\n');
 
-    content = `${content}\n\n<!--\n@component\n[Go to Popover](https://flowbite-svelte.com/)\n## Props\n${propComment}\n-->\n`;
+      content = `${content}\n\n<!--\n@component\n[Go to Popover](https://flowbite-svelte.com/)\n## Props\n${propComment}\n-->\n`;
 
-    fs.writeFileSync(filePath, content, 'utf-8');
-  }
-};
-
-const traverseDirectory = (dirPath) => {
-  const items = fs.readdirSync(dirPath);
-  for (const item of items) {
-    const itemPath = path.join(dirPath, item);
-    const stat = fs.statSync(itemPath);
-
-    if (stat.isDirectory()) {
-      traverseDirectory(itemPath); // Recurse into subdirectories
-    } else if (stat.isFile() && item.endsWith('.svelte')) {
-      processFile(itemPath); // Process files
+      fs.writeFileSync(filePath, content, 'utf-8');
     }
-  }
-};
+  };
 
-traverseDirectory(srcDir);
+  const traverseDirectory = (dirPath) => {
+    const items = fs.readdirSync(dirPath);
+    for (const item of items) {
+      const itemPath = path.join(dirPath, item);
+      const stat = fs.statSync(itemPath);
 
-console.log("Generating documentation...");
+      if (stat.isDirectory()) {
+        traverseDirectory(itemPath); // Recurse into subdirectories
+      } else if (stat.isFile() && item.endsWith('.svelte')) {
+        processFile(itemPath); // Process files
+      }
+      console.log('Completed: ',item)
+    }
+  };
+  console.log("Generating documentation...");
+  traverseDirectory(srcDir);
+  console.log("All done!");
 };
 
 
